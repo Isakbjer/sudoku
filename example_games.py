a9222e34b4f5867a8cd591198ac7417e1d21d8da
Isak Bjerkem <isakbjerkem@gmail.com>
2026-05-15 16:39:04 +0200

    added a file to place the example games

---
+++ example_games.py
@@ -0,0 +1,9 @@
+example_grid_1 = [[None, None, 3, 5, 9, 7, 2, None, 8],
+                    [2, 4, 5, None, 1, 8, 9, 3, 7],
+                    [7, 9, 8, None, None, None, 6, 5, None],
+                    [None, 8, 2, 4, None, 6, 1, 7, 3],
+                    [3, None, None, 1, 8, 9, 5, None, None],
+                    [6, 5, 1, None, 7, None, None, 9, None],
+                    [5, None, 6, 7, 2, None, 4, 8, 9],
+                    [None, 2, 7, 9, 4, 5, 3, 1, 6],
+                    [4, None, 9, None, 6, 3, None, 2, 5]]
